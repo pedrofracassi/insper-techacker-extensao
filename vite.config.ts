ed45cf097b1a5fe547b875878aa44fd9ec4641f1
Pedro Fracassi <pedroluizfracassi@gmail.com>
2024-10-24 23:48:07 -0300

    zip build

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.config";
+import zipPack from "vite-plugin-zip-pack";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+  plugins: [react(), crx({ manifest }), zipPack()],
   build: {
     rollupOptions: {
-      input: {
-        setup: "src/setup/index.html",
-        dashboard: "src/dashboard/index.html",
-        login: "src/login/index.html",
-        popup: "src/popup/index.html",
-        addon: "src/addon/index.html",
-        upload: "src/upload/index.html",
-        offscreen: "src/offscreen/index.html",
-      },
+      input: "index.html",
     },
   },
 });
